c7fae58b9b32c64c475bb9255e58af27c4808f8b
Allu Ketan Satya Sai <allusai2007@gmail.com>
2025-12-17 13:13:33 +0530

    Update responses and logging in index.js

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.get("/",(req, res)=>{
+    //console.log(req.rawHeaders);
+    res.send("<h1>Hello Stalker 😇</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+app.get("/about",(req,res)=>{
+    res.send("<h1>About me</h1><p></p>");
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+app.get("/contact",(req,res)=>{
+    res.send("<h1>u ain't getting my number 😛");
 });
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(port,()=> {
+    console.log(`The server is running on port ${port}`);
 });
